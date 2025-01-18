20ba5aea10327e9b663347eb508b4fbbc05f189b
leconstantin <lcon69184@gmail.com>
2025-01-18 16:20:43 +0200

    added raect hook with zod

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -7,13 +7,15 @@ const fileSchema = z.instanceof(File, { message: "Required" });
 const imageSchema = fileSchema.refine(
   (file) => file.size === 0 || file.type.startsWith("image/")
 );
-const addSchema = z.object({
+export const addSchema = z.object({
   name: z.string().min(1),
   description: z.string().min(1),
   priceInCents: z.coerce.number().int().min(1),
   file: fileSchema.refine((file) => file.size > 0, "Required"),
   image: imageSchema.refine((file) => file.size > 0, "Required"),
 });
+
+export type TaddSchema = z.infer<typeof addSchema>;
 export async function addProduct(formData: FormData) {
   //   console.log(formData);
 
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,60 +6,96 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
-import { addProduct } from "../../_actions/products";
+import { addProduct, addSchema, TaddSchema } from "../../_actions/products";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ProductForm() {
-  const [priceInCents, setPriceInCents] = useState<number>();
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [file, setFile] = useState<File>();
-  const [image, setImage] = useState();
+  //   const [priceInCents, setPriceInCents] = useState<number>();
+  //   const [name, setName] = useState<string>("");
+  //   const [description, setDescription] = useState<string>("");
+  //   const [file, setFile] = useState<File | undefined>();
+  //   const [image, setImage] = useState();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<TaddSchema>({
+    resolver: zodResolver(addSchema),
+  });
+  const onSubmit = async (data: TaddSchema) => {
+    console.log(data);
+    reset();
+  };
   return (
-    <form action={addProduct} className="space-y-6">
+    <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input
+          {...register("name")}
           type="text"
           id="name"
           name="name"
           required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
         />
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="priceInCents">Price In Cents</Label>
         <Input
+          {...register("priceInCents")}
           type="number"
           id="priceInCents"
           name="priceInCents"
           required
-          value={priceInCents}
-          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+          //   value={priceInCents}
+          //   onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
-        <div className="text-muted-foreground">
+        {errors.priceInCents && (
+          <p className="text-red-500">{errors.priceInCents.message}</p>
+        )}
+        {/* <div className="text-muted-foreground">
           {formatCurrency((priceInCents || 0) / 100)}
-        </div>
+        </div> */}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea
+        <textarea
+          {...register("description")}
           id="description"
           name="description"
           required
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
         />
+        {errors.description && (
+          <p className="text-red-500">{errors.description.message}</p>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input
+          {...register("file")}
+          type="file"
+          id="file"
+          name="file"
+          required
+        />
+        {errors.file && <p className="text-red-500">{errors.file.message}</p>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input
+          {...register("image")}
+          type="file"
+          id="image"
+          name="image"
+          required
+        />
+        {errors.image && <p className="text-red-500">{errors.image.message}</p>}
       </div>
-      <Button type="submit">Save</Button>
+      <Button type="submit" disabled={isSubmitting}>
+        Save
+      </Button>
     </form>
   );
 }
