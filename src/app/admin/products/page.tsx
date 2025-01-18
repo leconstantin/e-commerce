91a5ca8c0a9f4028a800f32d7b68f02bcf55d281
leconstantin <lcon69184@gmail.com>
2025-01-18 13:42:29 +0200

    added products page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
 import PageHeader from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableHead } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function AdminProductsPage() {
   return (
@@ -12,13 +18,27 @@ export default function AdminProductsPage() {
           <Link href="/admin/products/new">Add Product</Link>
         </Button>
       </div>
+      <ProductsTable />
     </>
   );
 }
 function ProductsTable() {
   return (
     <Table>
-      <TableHead></TableHead>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody></TableBody>
     </Table>
   );
 }
