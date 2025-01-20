81c86662a0afdeba781c6b659e8a75006378b8ef
leconstantin <lcon69184@gmail.com>
2025-01-20 18:58:06 +0200

    working on checkout form

---
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+type TcheckoutFormProps = {
+  product: {};
+  clientSecret: string;
+};
+export default function CheckoutForm({
+  product,
+  clientSecret,
+}: TcheckoutFormProps) {
+  return <div>CheckoutForm</div>;
+}
